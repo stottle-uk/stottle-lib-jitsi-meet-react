e60c73ce4da51986582bc74298080a8146ead3fc
Stuart Tottle <stuarttottle@hotmail.com>
2021-08-15 18:44:37 +0100

    fix muted state

--- src/conference/hooks/useMutedState.ts
+++ src/conference/hooks/useMutedState.ts
@@ -3,7 +3,7 @@ import { fromEvent, merge } from 'rxjs';
 import { TRACK_MUTE_CHANGED } from '../models/events/track';
 import { JitsiTrack } from '../models/JitsiTrack';
 
-export const useMutedSate = (video: JitsiTrack, audio: JitsiTrack) => {
+export const useMutedSate = (audio: JitsiTrack, video: JitsiTrack) => {
   const [isMuted, setMuted] = useState({
     audio: audio.isMuted(),
     video: video.isMuted()
@@ -13,9 +13,9 @@ export const useMutedSate = (video: JitsiTrack, audio: JitsiTrack) => {
     const sub = merge(
       fromEvent<JitsiTrack>(audio, TRACK_MUTE_CHANGED),
       fromEvent<JitsiTrack>(video, TRACK_MUTE_CHANGED)
-    ).subscribe(track => {
-      setMuted(state => ({ ...state, [track.getType()]: track.isMuted() }));
-    });
+    ).subscribe(track =>
+      setMuted(state => ({ ...state, [track.getType()]: track.isMuted() }))
+    );
 
     return () => sub.unsubscribe();
   }, [audio, video]);
